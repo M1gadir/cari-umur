c7ed690de2314c5851bda9856b4d373e62a062fa
My Macbook <mushabyushari@outlook.co.id>
2018-10-17 21:01:18 +0700

    v2 karena blom update dari xcode

--- cari umur/ViewController.swift
+++ cari umur/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var txtumur: UITextField!
+    @IBOutlet weak var lblhasil: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func btnumur(_ sender: Any) {
+        // deklarasi string dan konversi
+        var tahunlahir = Int(txtumur.text!)
+        // untuk mencari tahun sekarang
+        let tahunsekarang = 2018 - tahunlahir!
+        //menampilkan pada text field
+        lblhasil.text = "umur kamu adalah \(tahunsekarang)"
+    }
 }
 
